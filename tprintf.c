e7856119e90a0f9023c21787e785c699dd97aaa5
creatorkeep <creatorkeep@sina.com>
2016-09-04 15:34:56 +0800

    fit chapter3 questions

--- tprintf.c
+++ tprintf.c
@@ -5,8 +5,9 @@ int main (void)
     int i;
     float x;
     i = 40;
-    x = 839.21f;
+   // x = 30.523f;
+   x = .0000009979;
     printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
-    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
+    printf("|%10.3f|%12.5e|%-6.2g|\n", x, x, x);
     return 0;
 }
